aa25a45a863ffb8518c24ff5beeee5d7e5702d43
lijin0919 <317395616@qq.com>
2018-02-22 20:22:19 +0800

    想死

--- src/main/java/com/zhaolong/statistical/controller/ExportController.java
+++ src/main/java/com/zhaolong/statistical/controller/ExportController.java
@@ -3,13 +3,18 @@ package com.zhaolong.statistical.controller;
 import com.zhaolong.statistical.entity.KeywordsRecord;
 import com.zhaolong.statistical.repository.DataListRepository;
 import com.zhaolong.statistical.service.ExportService;
+import com.zhaolong.statistical.util.EmailUtil;
+import org.hibernate.validator.constraints.Email;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.mail.MessagingException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.*;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -24,6 +29,10 @@ public class ExportController {
 
     @Autowired
     private ExportService exportService;
+
+    @Autowired
+    private HttpSession session;
+
     @PostMapping("/exportexcl")
     public void exportData(@RequestParam("start-date")String start,
                            @RequestParam("end-date")String end,
@@ -35,6 +44,8 @@ public class ExportController {
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
         Date startDate = simpleDateFormat.parse(start);
         Date endDate = simpleDateFormat.parse(end);
+        session.setAttribute("start",startDate);
+        session.setAttribute("end",endDate);
 
         String path = exportService.exportExcl(startDate,endDate);
 
@@ -53,6 +64,40 @@ public class ExportController {
         }
         in.close();
         out.close();
+        //*******************************************
+
+
+
+    }
+
+
+
+    @GetMapping("/send")
+    public void send(HttpServletResponse response,HttpServletRequest request) throws ParseException, IOException, MessagingException {
+
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Date startDate = (Date) session.getAttribute("start");
+        Date endDate = (Date) session.getAttribute("end");
+
+        String path = exportService.exportExcl(startDate,endDate);
+
+
+        response.setContentType("multipart/form-data");
+        //设置Content-Disposition
+        response.setHeader("Content-Disposition", "attachment;filename=test.xls");
+
+        OutputStream out = response.getOutputStream();
+
+        InputStream in = new FileInputStream(new File(path));
+
+        int b;
+        while((b=in.read())!=-1){
+            out.write(b);
+        }
+        in.close();
+        out.close();
+        EmailUtil.sendFileEmail("317395616@qq.com","关键词数据","",new File(path));
         //*******************************************
 
 
